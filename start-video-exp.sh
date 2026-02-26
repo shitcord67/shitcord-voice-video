864b2c6d313d92b2a74c081f54b42a6b02a33063
ElElEm <elelem@users.noreply.github.com>
2026-02-26 13:20:49 +0100

    Fix video launcher to accept global args like --config before ctui

--- start-video-exp.sh
+++ start-video-exp.sh
@@ -11,16 +11,40 @@ if [[ ! -x "$VENV_PY" ]]; then
 fi
 
 LOG_FILE="${VIDEO_EXP_LOG:-./video-exp.log}"
-TOKEN_ARG=()
+GLOBAL_ARGS=()
+CTUI_EXTRA_ARGS=()
+
 if [[ -n "${TOKEN:-}" ]]; then
-  TOKEN_ARG=(--token "$TOKEN")
+  GLOBAL_ARGS+=(--token "$TOKEN")
 fi
 
-exec "$VENV_PY" selfbot_voice.py "${TOKEN_ARG[@]}" \
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --config|--token|--log-file)
+      if [[ $# -lt 2 ]]; then
+        echo "Missing value for $1" >&2
+        exit 1
+      fi
+      GLOBAL_ARGS+=("$1" "$2")
+      shift 2
+      ;;
+    --)
+      shift
+      CTUI_EXTRA_ARGS+=("$@")
+      break
+      ;;
+    *)
+      CTUI_EXTRA_ARGS+=("$1")
+      shift
+      ;;
+  esac
+done
+
+exec "$VENV_PY" selfbot_voice.py "${GLOBAL_ARGS[@]}" \
   --log-file "$LOG_FILE" \
   ctui \
   --dave-debug \
   --dave-wait-timeout 20 \
   --exp-self-video \
   --exp-video-opcode \
-  "$@"
+  "${CTUI_EXTRA_ARGS[@]}"
